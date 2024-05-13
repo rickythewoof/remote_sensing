b8975a2b783c172f26201cc53ada7ebde92902c8
Ricky <riccardo.zucchelli@gmail.com>
2024-05-13 11:39:32 +0200

    Changed readme.md, added requirements.txt and made a working PoC to test cuda functionality with torch

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
 # Repository for remote sensing work
+Repository made for Artificial Intelligence & Neural Network course with Prof. Ciarfuglia @ Sapienza 
 
-### Workflow
-Using python
-install dependencies with 
-```sudo apt install awscli && aws configure ```
-```pip  install numpy pandas matplotlib scipy sklearn ```
-
-
+### Install required packages
+Install everything with 
+    sudo apt install awscli python3 python3-pip && aws configure 
+    pip install -r src/requirements.txt
 
 ### Get Dataset
-
 _it's BIG, don't use mobile data_
 
     aws s3 cp s3://spacenet-dataset/spacenet/SN6_buildings/tarballs/SN6_buildings_AOI_11_Rotterdam_train.tar.gz .
 
 ### Sources
 [Spacenet AI](https://spacenet.ai/sn6-challenge/)
+[Neural Network lessons](https://youtube.com/playlist?list=PLkt2uSq6rBVctENoVBg1TpCC7OQi31AlC)
--- src/sensing.py
+++ src/sensing.py
@@ -1,9 +1,22 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import scipy.optimize as opt
-from sklearn import linear_model
-import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, SubsetRandomSampler, ConcatDataset
+from torchvision import datasets, transforms
 
-os.chdir("../")
-df = pd.read_csv("data/stockats.tar.gz")
+import numpy as np
+import sklearn
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+# This is when we use GPU instance to run colab. 
+# How to change Runtime in colab?? 
+# Go to Runtime-> Change Runtime type -> Hardware Accelerator -> GPU
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+    
+print('Using PyTorch version:', torch.__version__, ' Device:', device)
